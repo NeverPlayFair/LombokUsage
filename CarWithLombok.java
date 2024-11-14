c7e9de715f6dcd56504e8bf5b01ddddc7b798d04
Bartosz Borek <bartoszborek00@gmail.com>
2024-11-14 20:11:46 +0100

    AddFiles

---
+++ CarWithLombok.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"model", "brand"}) 
+public class CarWithLombok {
+    private String model;
+    private String brand;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
+}
